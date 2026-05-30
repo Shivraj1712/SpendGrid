f0c042c78211e831fc07e9b36ad187b0090c5688
Shivraj1712 <shivrajmaharaul688@gmail.com>
2026-05-30 09:16:00 +0530

    Updating the baseURL with the deployed server on render

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api/v0",
+  baseURL: "https://spendgrid-2.onrender.com/api/v0",
   withCredentials: true,
 });
 
